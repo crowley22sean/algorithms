948a65759af91ce1e161005958f6f1f36a7c34e8
Sean Crowley <seancrowley499@gmail.com>
2023-11-07 09:30:03 -0500

    initial commit

---
+++ algorithm-visualizer/src/app/sorting-visualizer/sorting-visualizer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sorting-visualizer',
+  templateUrl: './sorting-visualizer.component.html',
+  styleUrls: ['./sorting-visualizer.component.css']
+})
+export class SortingVisualizerComponent {
+
+}
